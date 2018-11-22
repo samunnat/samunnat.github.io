a1d87b2c0b10162f9b48d5847788b3fd7e2b3fb8
samunnat <sala6632@colorado.edu>
2018-11-21 21:57:18 -0700

    Experimenting with states

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
+import Projects from './Components/Projects'
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      projects: []
+    }
+  }
+
+  componentWillMount(){
+    this.setState({
+      projects: [
+        {
+          title: 'JavaScript Chesss',
+          category: 'Web Design'
+        },
+        {
+          title: 'TwitHits',
+          category: 'App Development'
+        }
+      ]
+    });
+  }
+
   render() {
     return (
       <div className="App">
         My Website
+        <Projects projects={this.state.projects}/>
       </div>
     );
   }
