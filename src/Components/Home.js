6b5a552ea2b6691e954aec5a184c4b45e27395e1
samunnat <sala6632@colorado.edu>
2018-12-15 05:35:42 -0700

    Added Intro block with social-media links

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,11 +3,27 @@ import { Button, Container, Divider, Grid, Header, Image, Menu, Segment, Icon } 
 
 class Home extends Component {
   render() {
+    // TODO: need to fix anchor tags since they overflow outside the icons
     return (
-        <Container>
-            <img src="/avatar.jpg" alt="avatar" className="ui avatar small image"/>
-            <Icon name="github" size="big"/>
-            <Icon name="linkedin" size="big"/>
+        <Container className='center aligned'>
+            <Image src="/avatar.jpg" alt="avatar" className="ui avatar small image"/>
+
+            <Header size='huge' style={{color: 'lightgray'}}>Samunnat Lamichhane</Header>
+
+            <div className="info_links">
+
+              <a href="https://github.com/samunnat" rel="noopener noreferrer" target="_blank">
+                <Icon name="github" color='violet' size="big" link />
+              </a>
+
+              <a href="/resume.pdf" rel="noopener noreferrer" target = "_blank">
+                <Button compact color='teal' size='tiny'>resume</Button>
+              </a>
+
+              <a href="https://www.linkedin.com/in/samunnatlamichhane" rel="noopener noreferrer" target="_blank">
+                <Icon name="linkedin" color='blue' size="big" link />
+              </a>
+            </div>
         </Container>
     );
   }
