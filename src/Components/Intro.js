6c44540acff3304ca7f0d3360e368d4e79a9b0c8
samunnat <sala6632@colorado.edu>
2019-02-04 05:54:15 -0700

    enabled avatar

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -5,7 +5,7 @@ import InfoLinks from './InfoLinks'
 const Intro = () => (
   <div id="Intro">
     <Container className='center aligned' style={{padding: '1em 0.5em 1em 0.5em'}}>
-        <Image src="/avatar.jpg" alt="avatar" className="ui avatar small image" disabled/>
+        <Image src="/avatar.jpg" alt="avatar" className="ui avatar small image"/>
 
         <Header as='h1' size='huge' style={{color: 'lightgray'}}>Sam Lamichhane</Header>
 
