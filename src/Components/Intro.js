90b4b5bfc940992ad3173168405022bc66a61cd7
samunnat <sala6632@colorado.edu>
2019-02-04 05:28:31 -0700

    Changing header margin

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -2,16 +2,16 @@ import React, { Component } from 'react';
 import { Button, Container, Header, Icon, Image } from 'semantic-ui-react'
 import InfoLinks from './InfoLinks'
 
-const About = () => (
+const Intro = () => (
   <div id="Intro">
-    <Container className='center aligned' style={{padding: '0.5em 0.5em 0.5em 0.5em'}}>
+    <Container className='center aligned' style={{padding: '1em 0.5em 1em 0.5em'}}>
         <Image src="/avatar.jpg" alt="avatar" className="ui avatar small image" disabled/>
 
-        <Header as='h1' style={{color: 'lightgray', margin: '0.45em', fontSize: "4em"}}>Sam Lamichhane</Header>
+        <Header as='h1' size='huge' style={{color: 'lightgray'}}>Sam Lamichhane</Header>
 
         <InfoLinks />
     </Container>
   </div>
 )
 
-export default About;
+export default Intro;
