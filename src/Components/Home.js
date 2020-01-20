f79f441119bbeead1765827ff5838b3ad71979a5
samunnat <sala6632@colorado.edu>
2020-01-19 23:58:51 -0700

    Fixed routing

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,40 +1,50 @@
 import React from 'react';
-import { Container, Header } from 'semantic-ui-react';
+import { Container, Header, List } from 'semantic-ui-react';
 import './css/Home.css';
 
 const Home = () => {
     return (
         <Container>
             <Header color='olive' inverted>
-                Ahoy-hoy! I'm a senior undergrad at CU Boulder majoring in CompSci. <br/><br/>
-                I'm interested in Full-Stack Web Dev, Big-Data Infrastructure, and Automation.
+                Ahoy-hoy! I'm a senior undergrad at CU Boulder majoring in CompSci.
             </Header>
-            <Header color='olive' inverted>
-                In my free time, I like to:
-                    <ul>
-                        <li>
-                            Read
-                            <ul>
-                                <li>Sci-Fi, Fantasy, Tech</li>
-                                <li>Favorites: 
-                                    <ul>
-                                        <li>Dune</li>
-                                        <li>A Storm of Swords</li>
-                                        <li>Clean Code</li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </li>
-                        <li>
-                            Hike
-                            <ul>
-                                <li>
-                                    anywhere with a view
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
+            <Header as='h3' color='olive' inverted>
+                I'm interested in Full-Stack Web Dev, Big-Data Architecture, and Automation.
             </Header>
+            <List color='olive' inverted>
+                <List.Header as={Header} color='olive' inverted>
+                    In my free time, I like to:
+                </List.Header>
+                <List.Item>
+                    <List.Header>Test</List.Header>
+                </List.Item>
+                <List.Item>
+                    <List.Header>Test2</List.Header>
+                </List.Item>
+            </List>
+                {/* <ul>
+                    <li>
+                        Read
+                        <ul>
+                            <li>Sci-Fi, Fantasy, Tech</li>
+                            <li>Favorites: 
+                                <ul>
+                                    <li>Dune</li>
+                                    <li>A Storm of Swords</li>
+                                    <li>Clean Code</li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        Hike
+                        <ul>
+                            <li>
+                                anywhere with a view
+                            </li>
+                        </ul>
+                    </li>
+                </ul> */}
         </Container>
     );
 }
--- src/Components/TabPages.js
+++ src/Components/TabPages.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, NavLink, matchPath } from 'react-router-dom';
 import { Tab, Container } from 'semantic-ui-react'
 import Home from './Home';
 import Projects from './Projects';
@@ -7,62 +8,109 @@ import Contact from './Contact';
 const panes = [
     { 
         menuItem: { 
+            as: NavLink,
             content: 'Home',
+            to: '/',
+            exact: true,
             key: 'Home', 
             icon: { name: 'hand spock', color: 'olive' }
         },
         render: () => (
-            <Tab.Pane color='black' inverted>
-                <Home/>
-            </Tab.Pane>
+            <Route path='/' exact>
+                <Tab.Pane color='black' inverted>
+                    <Home/>
+                </Tab.Pane>
+            </Route>
         )
     },
     { 
         menuItem: { 
+            as: NavLink,
             content: 'Projects',
+            to: '/projects',
             key: 'Projects', 
             icon: { name: 'code branch', color: 'yellow' }
         },
         render: () => (
-            <Tab.Pane color='black' inverted> 
-                <Projects/>
-            </Tab.Pane>
+            <Route path='/projects'>
+                <Tab.Pane color='black' inverted> 
+                    <Projects/>
+                </Tab.Pane>
+            </Route>
         )
     },
     { 
         menuItem: {
+            as: NavLink,
             content: 'Contact',
+            to: '/contact',
             key: 'Contact',
             icon: { name: 'paper plane', color: 'orange' }
         },
         render: () => (
-            <Tab.Pane color='black' inverted>
-                <Contact/> 
-            </Tab.Pane>
+            <Route path='/contact'>
+                <Tab.Pane color='black' inverted>
+                    <Contact/> 
+                </Tab.Pane>
+            </Route>
         )
     },
 ];
 
-const TabPages = () => {
-    return (
-        <Container>
-                <Tab 
-                    defaultActiveIndex={0}
-                    menu={{ 
-                        fluid: false,
-                        attached: false,
-                        tabular: false,
-                        size: 'large',
-                        inverted: true,
-                        widths: 3,
-                        borderless: true,
-                        color: 'black'
-                    }} 
-                    size='large'
-                    panes={panes}
-                />
-        </Container>
-    );
+class TabPages extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeTab: this.getCurrentTabIndex(),
+        }
+    }
+
+    handleTabChange = (e, { activeIndex }) => this.setState({ activeTab: activeIndex })
+
+    componentDidUpdate() {
+        window.onpopstate = (e) => {
+            this.setState({
+                activeTab: this.getCurrentTabIndex()
+            })
+        }
+    }
+
+    getCurrentTabIndex() {
+        let x = panes.findIndex(pane => {
+            return !!matchPath(window.location.pathname, {
+                path: pane.menuItem.to,
+                exact: true,
+                strict: true
+            });
+        });
+        console.log(x);
+        return x;
+    }
+
+    render() {
+        return (
+            <Container>
+                <Router basename={process.env.PUBLIC_URL}>
+                    <Tab 
+                        activeIndex={this.state.activeTab}
+                        menu={{ 
+                            fluid: false,
+                            attached: false,
+                            tabular: false,
+                            size: 'large',
+                            inverted: true,
+                            widths: 3,
+                            borderless: true,
+                            color: 'black',
+                        }} 
+                        size='large'
+                        panes={panes}
+                        onTabChange={this.handleTabChange}
+                    />
+                </Router>
+            </Container>
+        );
+    }
 }
 
 export default TabPages;
